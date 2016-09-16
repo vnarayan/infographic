415bfba34c47321e10cee2537ec661eb1cdc73be
Øystein Bedin <oybed@users.noreply.github.com>
2016-09-15 23:30:24 -0600

    Update ReleaseJenkinsfile.groovy

--- ReleaseJenkinsfile.groovy
+++ ReleaseJenkinsfile.groovy
@@ -1,11 +1,6 @@
 releasePipeline{
-    buildCommands = [] 
+    buildCommands = ['./build.sh']
     ocHost = 'master1.env2-1.innovation.labs.redhat.com'
     dockerRegistry = 'registry.apps.env2-1.innovation.labs.redhat.com'
-    appName = 'web'
-    envs = [
-        [name: 'Dev', projectName: 'ig-dev'],
-        [name: 'Stage', projectName: 'ig-stage' ],
-        [name: 'Demo', projectName: 'ig-demo' ]
-   ]
+    appName = 'infographic'
 }
